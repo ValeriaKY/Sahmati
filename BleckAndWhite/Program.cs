27e544563a7b68cf8ea3adce84af405992385c93
Егор хуй попадёшь <62738342+ValeriaKY@users.noreply.github.com>
2020-04-16 09:53:56 +0300

    Добавьте файлы проекта.

--- BleckAndWhite/Program.cs
+++ BleckAndWhite/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,7 +19,7 @@ namespace ZMEIKKA
            do
            {
                 XYI.Figura_vibor();
-                //XYI.AI_Figura_vibor();
+                XYI.AI_Figura_vibor();
            } while (true);
         }
     }
